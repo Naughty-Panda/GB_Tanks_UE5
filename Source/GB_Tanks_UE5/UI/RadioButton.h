6f5b1fd265681aac13a7f3e47e4d6d8def644e15
Naughty Panda <hateme1990@gmail.com>
2021-09-28 22:39:32 +0300

    Added Slate and UMG RadioButton classes.

---
+++ Source/GB_Tanks_UE5/UI/RadioButton.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "Slate/SRadioButton.h"
+
+#include "RadioButton.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnButtonStateChangedEvent, ERadioButton, ButtonId);
+
+/**
+ * 
+ */
+UCLASS()
+class GB_TANKS_UE5_API URadioButton : public UWidget
+{
+	GENERATED_BODY()
+
+protected:
+	TSharedPtr<SRadioButton> RadioButton;
+
+	UPROPERTY(BlueprintAssignable, Category="UI Events")
+	FOnButtonStateChangedEvent OnButtonStateChangedEvent;
+
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+
+	void OnButtonStateChanged(ERadioButton ButtonId);
+};
