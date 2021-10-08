9cff8ae752a30876dfd2dbb3e2db53b2ffd32c1e
Naughty Panda <hateme1990@gmail.com>
2021-10-09 00:16:19 +0300

    Added basic StyleSet files.

---
+++ Source/GB_Tanks_UE5/UI/Slate/StyleSet.cpp
@@ -0,0 +1,41 @@
+#include "StyleSet.h"
+#include "Engine.h"
+#include "SlateBasics.h"
+#include "Slate/SlateGameResources.h"
+#include "Styling/ISlateStyle.h"
+
+TSharedPtr<ISlateStyle> FindStyle(const FString& ScopeToDirectory)
+{
+    TSharedPtr<FSlateGameResources> Style = FSlateGameResources::New(FName("StyleSet"), ScopeToDirectory, ScopeToDirectory);
+
+    return Style;
+}
+
+TSharedPtr<ISlateStyle> FStyleSet::StylePtr = nullptr;
+
+void FStyleSet::Initialize()
+{
+    Shutdown();
+
+    StylePtr = FindStyle("/Game/UI/Styles");
+    FSlateStyleRegistry::RegisterSlateStyle(*StylePtr);
+}
+
+void FStyleSet::Shutdown()
+{
+    if (StylePtr.IsValid())
+    {
+		FSlateStyleRegistry::UnRegisterSlateStyle(*StylePtr);
+		ensure(StylePtr.IsUnique());
+		StylePtr.Reset();
+    }
+}
+
+const ISlateStyle& FStyleSet::Get()
+{
+    if (!StylePtr.IsValid())
+    {
+		Initialize();
+    }    
+    return *StylePtr;
+}
